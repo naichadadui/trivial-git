f269ecd50edfadf053494e047d64bc9c2b6d8623
Helen <sei_hlz@126.com>
2018-01-12 06:37:52 +0800

    还原ControllerTest

--- src/test/java/com/ecnu/trivial/controller/api/GameControllerTest.java
+++ src/test/java/com/ecnu/trivial/controller/api/GameControllerTest.java
@@ -2,49 +2,38 @@ package com.ecnu.trivial.controller.api;
 
 import com.ecnu.trivial.TrivialApplication;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockServletContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import static org.junit.Assert.*;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = TrivialApplication.class)
-@WebAppConfiguration
+@SpringBootTest(classes = {TrivialApplication.class, MockServletContext.class})
 public class GameControllerTest {
+
+    @Autowired
+    private WebApplicationContext context;
     @Test
     public void enterRoom() throws Exception {
-
+        MockMvc mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
+        String uri = "/api/game/enterRoom";
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.post(uri).param("roomId", "1").accept(MediaType.APPLICATION_JSON)).andReturn();
+        String result=mvcResult.getResponse().getContentAsString();
+        Assert.assertEquals(result,"{\"returnMessage\":\"success\",\"enterResult\":0}");
     }
-
-    @Test
-    public void leaveRoom() throws Exception {
-
-    }
-
-    @Test
-    public void ready() throws Exception {
-
-    }
-
-    @Test
-    public void answerQuestion() throws Exception {
-
-    }
-
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @After
-    public void tearDown() throws Exception {
-
-    }
-
 
 }
